466a37c52acc207bb906274ee63e1a6cfee6ebfd
Miro <MiroPhoenix91@gmail.com>
2021-09-02 21:16:01 +0200

    modified

--- app.js
+++ app.js
@@ -7,20 +7,3 @@
 // function addBookToLibrary() {
     
 // }
-
-fetch("https://google-books.p.rapidapi.com/volumes?key=AIzaSyAOsteuaW5ifVvA_RkLXh0mYs6GLAD6ykc", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "google-books.p.rapidapi.com",
-		"x-rapidapi-key": "7ecbb014b0mshc95dbe51b85f5d9p186b56jsn682d5e35fb85"
-	}
-})
-.then(response => {
-	return response.json();
-})
-.then(data => {
-    console.log(data)
-})
-.catch(err => {
-	console.error(err);
-});
